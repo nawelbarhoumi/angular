3380321eae0747c966344120e658df8f2e381c7c
nawelbarhoumi <nawel.barhoumi@gmail.com>
2021-03-23 15:58:57 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
+import { LoginComponent } from './login/login.component';
+import { Page404Component } from './page404/page404.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateTodoComponent } from './update-todo/update-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'todo',
+    component: AddTodoComponent
+  },
+  {
+    path: 'update-todo/:index',
+    component: UpdateTodoComponent
+  },
+  {
+    path: 'add-todo',
+    component: AddTodoComponent
+  },
+  {
+    path: 'todo-list',
+    component: ListTodoComponent
+  },
+  {
+    path: 'add-product',
+    component: AddProductComponent
+  },
+  {
+    path: 'list-product',
+    component: ListProductComponent
+  },
+  {
+    path:'**',
+    component: Page404Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +7,36 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { Page404Component } from './page404/page404.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
+import { UpdateTodoComponent } from './update-todo/update-todo.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterComponent,
+    Page404Component,
+    AddTodoComponent,
+    ListTodoComponent,
+    UpdateTodoComponent,
+    AddProductComponent,
+    ListProductComponent,
+    UpdateProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatInputModule
   ],
